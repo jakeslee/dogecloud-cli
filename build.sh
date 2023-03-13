0346181247aca6392e0e1fce8d5a22b735ac1ed9
Jakes Lee <jakeslee66@gmail.com>
2023-03-13 15:33:41 +0800

    add multiplatform build script

--- build.sh
+++ build.sh
@@ -6,4 +6,11 @@ flags="-X github.com/jakeslee/dogecloud-cli/cmd.BuildTime=$(date -u '+%Y-%m-%d_%
 export CGO_ENABLED=0
 
 echo "building version: $version"
-go build -ldflags "$flags" -o ./output/dogecloud-cli main.go
+
+for os in "linux" "darwin" ; do
+  for arch in "arm64" "amd64" ; do
+    echo "building for $os/$arch"
+    GOOS="$os" GOARCH="$arch" go build -ldflags "$flags" -o "./output/dogecloud-cli-$os-$arch" main.go
+    chmod +x "./output/dogecloud-cli-$os-$arch"
+  done
+done
